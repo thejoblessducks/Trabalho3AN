b012a1e571fede128d2a27451b47c2137daa78c5
eamorgado <edu.dado99@gmail.com>
2019-04-27 23:04:06 +0100

    Update

--- NewtonDiferences.py
+++ NewtonDiferences.py
@@ -1,11 +1,16 @@
 from __future__ import division
 import numpy as np
-
+'''-----------------------------------------------------------------------------
+Newton diferences class
+    Calculates the divided diferences 
+    Calculates the polinomial aproximation for a value
+-----------------------------------------------------------------------------'''
 class NewtonDiferences():
     def __init__(self,x,y):
         self.x = x
         self.y = y
     def dividedDiferences(self):
+        #Calculates all the divided diferences for the list of points
         x = self.x
         y = self.y
         n = len(x)
@@ -18,6 +23,7 @@ class NewtonDiferences():
                 a[i] = (a[i]-a[i-1])/(x[i]-x[i-j])
         return np.array(a)
     def calInterpolation(self,a,x,value_to_interpolate):
+        #given a value_to_interpolate aproximates the value through the polinomial
         n = len(a)-1
         tmp = a[n]
         for i in range(n-1,-1,-1):
